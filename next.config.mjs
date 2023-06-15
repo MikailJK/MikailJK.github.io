232a49f340d91f47f7f260f9138644d6229d7a7d
mjkrocht <mjkrocht@ncsu.edu>
2023-06-15 11:55:47 -0400

    Updated for Vercel Publishing

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,7 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig   = {
-  output: 'export',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   assetPrefix: './',
 };
-
   
   export default nextConfig;
